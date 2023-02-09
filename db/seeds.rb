97cf01b1721ce84f2a52ecec90b7df010373cbab
Sebastiano Labbate <seb.94@hotmail.co.uk>
2023-02-09 18:36:09 +0000

    bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+
+api = '246c25583f1579de9a8d2f1825bd140d'
+
+res = URI.open("https://api.themoviedb.org/3/movie/popular?api_key=#{api}&language=en-US&page=1").read
+data = JSON.parse(res)
+
+data['results'].first(10).each do |result|
+  Movie.create!(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result['poster_path']}",
+    rating: result['vote_average']
+  )
+end

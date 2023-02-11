3739962e84bef51ce5ce885470663b460373d736
Sebastiano Labbate <seb.94@hotmail.co.uk>
2023-02-11 12:38:55 +0000

    added comments to movies

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
   def index
     @lists = List.all
   end
@@ -7,22 +9,29 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+  def show; end
 
   def create
     @list = List.new(list_params)
-    if @list.save
+    if @list.save!
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :photo)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
